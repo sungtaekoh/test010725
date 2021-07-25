1a8c6b8e033340b14b33bbaac0613f3bedcc7a96
sungtaek <ohbass95@gmail.com>
2021-07-25 23:39:22 +0900

    test03

---
+++ src/test010725/testQ010725.java
@@ -0,0 +1,17 @@
+package test010725;
+
+public class testQ010725 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("=======================================");
+		System.out.println("이름\t나이\t전화번호\t\t회비");
+		System.out.println("=======================================");
+		System.out.println("홍길동\t"+"\""+15+"\""+"\t"+3672+"-"+1234+"\t\\"+2000);
+		System.out.println("홍길동\t\"15\"\t3672-1234\t\\20000");
+		System.out.println("11\"15\"11");
+		System.out.println("");
+		System.out.println("");
+		
+	}
+}
